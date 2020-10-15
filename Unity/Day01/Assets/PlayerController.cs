285785567721e24eda164602cec04991da9618e9
ㄱㅈㅇ <xodlf6604@naver.com>
2020-10-15 15:42:00 +0900

    d2

---
+++ Unity/Day01/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontal * Time.deltaTime * speed, vertical * Time.deltaTime * speed, 0f);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+    }
+}
