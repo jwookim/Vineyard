2ddef0a9fb1f3f4adf8dbd29a6f2150d6cd3c085
ㄱㅈㅇ <xodlf6604@naver.com>
2020-10-26 16:00:33 +0900

    d9

---
+++ Unity/Day 09/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    [SerializeField] private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", vertical);
+
+
+        //                  (0f, 0f, 1f)    * -1 ~ 1   *    0.02f            *  5f
+        transform.Translate(Vector3.forward * vertical * Time.fixedDeltaTime * speed);
+
+
+        transform.Rotate(Vector3.up * horizontal, Space.World);
+    }
+}
